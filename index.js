a1958836b710fadb36251d05c2bc5d5baf698c7d
Axel <axel.gallardo.e@gmail.com>
2024-01-28 23:01:49 -0500

    acabo de lograr que se abra react native en el celular

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// index.js
+import { AppRegistry } from 'react-native';
+import App from './App'; // O el nombre del componente principal de tu aplicación
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (window.document) {
+  const rootTag = document.getElementById('root') || document.getElementById('app') || document.getElementById('main');
+  AppRegistry.runApplication(appName, { rootTag });
+},
